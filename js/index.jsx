8e4515356d7aeaa78a52166cfb32d9bc8416f26a
Adrien Crivelli <adrien.crivelli@gmail.com>
2020-07-01 13:11:04 +0900

    Drop endpoint button
    Because queries are not replayed automatically when endpoint changes,
    the button actually has no effect at all. And it could be confusing
    to see an endpoint in input that was not yet applied via the button
    with a mis-matching query results.
    
    Removing the button entirely, avoid confusing states.

--- js/index.jsx
+++ js/index.jsx
@@ -1,73 +1,74 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
 import ReactDOM from 'react-dom';
 import GraphiQL from 'graphiql';
+import useDebounce from './use-debounce';
 import 'graphiql/graphiql.css';
 
+function graphQLFetcher(endpoint) {
+    return function (graphQLParams) {
+        return fetch(endpoint, {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(graphQLParams),
+            credentials: 'include',
+        }).then(response => response.json());
+    }
+}
+
 function GraphiQLExtension(props) {
-    const [textValue, setTextValue] = useState(props.endpoint);
-    const [currEndpoint, setCurrEndpoint] = useState(props.endpoint);
+    const [endpoint, setEndpoint] = useState(props.endpoint);
+    const [isTyping, setIsTyping] = useState(false);
+    const debouncedEndpoint = useDebounce(endpoint, 500);
 
-    const graphQLFetcher = (endpoint) => {
-        return function (graphQLParams) {
-            return fetch(endpoint, {
-                method: 'post',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify(graphQLParams),
-                credentials: 'include',
-            }).then(response => response.json());
-        }
-    }
-    
-    const handleKeyDown = (e) => {
-        if (e.key === 'Enter') {
-            setEndpoint();
-        }
-    }
+    // Store endpoint in local storage only after debounced
+    useEffect(
+        () => {
+            setIsTyping(false);
+            chrome.storage.local.set({
+                endpoint: debouncedEndpoint,
+            });
+        },
+        [debouncedEndpoint],
+    );
 
-    const setEndpoint = () => {
-        chrome.storage.local.set(
-            {'endpoint': textValue},
-            () => {
-                if (!chrome.runtime.lastError) {
-                    // Move current endpoint to previous, and set current endpoint to new.
-                    setCurrEndpoint(textValue);
-                }
-            },
-        );
+    function changed(event) {
+        setIsTyping(true);
+        const newEndpoint = event.target.value;
+        setEndpoint(newEndpoint);
     }
 
-    const updateEndpoint = (e) => {
-        setTextValue(e.target.value);
-    }
+    // Only re-render GraphiQL when debounced endpoint actually changed, otherwise
+    // it would re-render and fetch introspection queries too often
+    const graphiQL = useMemo(() => {
+        return debouncedEndpoint ? (
+            <GraphiQL
+                id="graphiql"
+                fetcher={graphQLFetcher(debouncedEndpoint)}
+            />
+        ) : (
+            <p id="no-endpoint">Set a non empty endpoint above</p>
+        )
+
+    }, [debouncedEndpoint]);
 
     return (
-      <div id="application">
-        <div id="url-bar" className="graphiql-container">
-          <input
-            type="text"
-            id="url-box"
-            defaultValue={props.endpoint}
-            onChange={updateEndpoint}
-            onKeyDown={handleKeyDown}
-          />
-          <a id="url-save-button" className="toolbar-button" onClick={setEndpoint}>
-            Set endpoint
-          </a>
+        <div id="application">
+            <div id="url-bar" className="graphiql-container">
+                <input
+                    type="url"
+                    id="url-box"
+                    defaultValue={props.endpoint}
+                    onChange={changed}
+                />
+                <div id="typing" style={isTyping ? {} : {visibility: 'hidden'}}>Typing ...</div>
+            </div>
+            {graphiQL}
         </div>
-        {currEndpoint ? (
-          <GraphiQL
-            id="graphiql"
-            fetcher={graphQLFetcher(currEndpoint)}
-          />
-        ) : (
-          <p id="no-endpoint">Set a non empty endpoint above</p>
-        )}
-      </div>
     );
 }
 
 chrome.storage.local.get('endpoint', (storage) =>
-    // Render <GraphiQL /> into the container.
+    // Render <GraphiQLExtension /> into the container.
     ReactDOM.render(
         <GraphiQLExtension endpoint={storage.endpoint}/>,
         document.getElementById('react-container'),
